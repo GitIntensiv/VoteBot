183a9c62b79c6febd1a36d7871da1d2b1466d3cb
olgafedorova <fedorova-olga.info@yandex.ru>
2020-06-24 19:45:43 +0300

    Last change

--- src/main/java/time/SchedulerMessage.java
+++ src/main/java/time/SchedulerMessage.java
@@ -17,8 +17,8 @@ public class SchedulerMessage extends TimerTask {
 
     public SchedulerMessage() {
         ZoneId zone = ZoneId.of("Europe/Moscow");
-        this.startRuffle = ZonedDateTime.of(LocalDateTime.of(2020, 6, 24, 20, 0, 0), zone);
-        this.endRuffle = ZonedDateTime.of(LocalDateTime.of(2020, 7, 30, 20, 0, 0), zone);
+        this.startRuffle = ZonedDateTime.of(LocalDateTime.of(2020, 6, 24, 19, 0, 0), zone);
+        this.endRuffle = ZonedDateTime.of(LocalDateTime.of(2020, 7, 30, 19, 0, 0), zone);
         this.isStartMessageSend = false;
         this.isEndMessageSend = false;
         this.bot = new Bot();
@@ -28,6 +28,7 @@ public class SchedulerMessage extends TimerTask {
     public void run() {
         ZoneId zone = ZoneId.of("Europe/Moscow");
         ZonedDateTime now = ZonedDateTime.of(LocalDateTime.now(), zone);
+        System.out.println(now);
         if (now.isAfter(startRuffle) && !isStartMessageSend) {
             bot.sendStartMessage();
             isStartMessageSend = true;
